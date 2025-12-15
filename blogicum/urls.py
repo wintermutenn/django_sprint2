96cb2b6700396141d9c0d601b23f88022b889123
Steve <58792824+wintermutenn@users.noreply.github.com>
2025-12-15 23:36:05 +0300

    Delete blogicum/urls.py

--- blogicum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
-    path('pages/', include('pages.urls', namespace='pages')),
-    path('admin/', admin.site.urls),
-]
